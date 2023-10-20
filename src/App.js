b93b6448afda3962e0c78f358152e2b8f47ce571
Rajat Gupta <dguptarajat@gmail.com>
2023-10-20 11:18:35 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> dmqkwd kw fk23mfk 23ekjofme2k
+          f2jk3mfko234nfgjo2ngj4n g4j
         </p>
         <a
           className="App-link"
@@ -15,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          wdkqwodkwqn jowqkd wiofdqwjk djqownrknoi
         </a>
       </header>
     </div>
