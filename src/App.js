5763ba8217e76b42aa0d77f3a02e9d1784aaace0
Rajat Gupta <dguptarajat@gmail.com>
2023-10-31 21:16:53 +0530

    print option

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useCallback, useMemo, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import axios from "axios";
 
 function App() {
+  const gridRefTerm = useRef();
+  const gridRefDisease = useRef();
   const [searchText, setSearchText] = useState("");
   const [termData, setTermData] = useState([]);
   const [diseaseData, setDiseaseData] = useState([]);
@@ -32,6 +34,15 @@ function App() {
     { field: "nk_count" },
   ];
 
+  const defaultColDef = useMemo(() => {
+    return {
+      editable: true,
+      resizable: true,
+      minWidth: 100,
+      flex: 1,
+    };
+  }, []);
+
   const handleKeyDown = async (event) => {
     if (event.key === "Enter") {
       if (searchText.trim() === "") {
@@ -50,6 +61,14 @@ function App() {
     }
   };
 
+  const onBtnExportTerm = useCallback(() => {
+    gridRefTerm.current.api.exportDataAsCsv();
+  }, []);
+
+  const onBtnExportDisease = useCallback(() => {
+    gridRefDisease.current.api.exportDataAsCsv();
+  }, []);
+
   return (
     <div className="w-100 h-full flex flex-col items-center">
       <input
@@ -61,41 +80,63 @@ function App() {
         required
         onKeyDown={handleKeyDown}
       />
-      <div className="self-start mt-16">
-        {termData?.length > 0 && (
-          <button
-            className={`${
-              active === 1
-                ? "bg-blue-500 text-white"
-                : "bg-transparent text-blue-700 border-blue-500"
-            } font-semibold py-2 px-4 rounded border`}
-            onClick={() => {
-              if (active === 2) setActive(1);
-            }}
-          >
-            Term Results
-          </button>
-        )}
-        {diseaseData?.length > 0 && (
-          <button
-            className={`${
-              active === 2
-                ? "bg-blue-500 text-white"
-                : "bg-transparent text-blue-700 border-blue-500"
-            } font-semibold py-2 px-4 rounded border`}
-            onClick={() => {
-              if (active === 1) setActive(2);
-            }}
-          >
-            Disease Results
-          </button>
-        )}
+      <div className="flex items-center justify-between w-full mt-16">
+        <div>
+          {termData?.length > 0 && (
+            <button
+              className={`${
+                active === 1
+                  ? "bg-blue-500 text-white"
+                  : "bg-transparent text-blue-700 border-blue-500"
+              } font-semibold py-2 px-4 rounded border`}
+              onClick={() => {
+                if (active === 2) setActive(1);
+              }}
+            >
+              Term Results
+            </button>
+          )}
+          {diseaseData?.length > 0 && (
+            <button
+              className={`${
+                active === 2
+                  ? "bg-blue-500 text-white"
+                  : "bg-transparent text-blue-700 border-blue-500"
+              } font-semibold py-2 px-4 rounded border`}
+              onClick={() => {
+                if (active === 1) setActive(2);
+              }}
+            >
+              Disease Results
+            </button>
+          )}
+        </div>
+        <div>
+          {active === 1 && termData?.length > 0 && (
+            <button
+              onClick={onBtnExportTerm}
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded border"
+            >
+              Download Term Results
+            </button>
+          )}
+          {active === 2 && diseaseData?.length > 0 && (
+            <button
+              onClick={onBtnExportDisease}
+              className="bg-blue-500 text-white font-semibold py-2 px-4 rounded border"
+            >
+              Download Disease Results
+            </button>
+          )}
+        </div>
       </div>
       {termData?.length > 0 && active === 1 && (
         <div className="ag-theme-alpine w-full h-full">
           <AgGridReact
+            ref={gridRefTerm}
             rowData={termData}
             columnDefs={termColDef}
+            defaultColDef={defaultColDef}
             style={{ height: "fit-content", width: "100%" }}
           ></AgGridReact>
         </div>
@@ -103,8 +144,10 @@ function App() {
       {diseaseData?.length > 0 && (active === 2 || termData?.length === 0) && (
         <div className="ag-theme-alpine w-full h-full">
           <AgGridReact
+            ref={gridRefDisease}
             rowData={diseaseData}
             columnDefs={diseaseColDef}
+            defaultColDef={defaultColDef}
             style={{ height: "fit-content", width: "100%" }}
           ></AgGridReact>
         </div>
